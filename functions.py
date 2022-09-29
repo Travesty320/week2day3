f0138d3d00bd2187d39c9e2177c52be51dc682aa
Travis Morris <thebosstee3@yahoo.com>
2022-09-29 18:14:16 -0400

    first

---
+++ functions.py
@@ -0,0 +1,16 @@
+# from operator import length_hint
+# from turtle import width
+
+def area():
+    l = int(input("Enter length:"))
+    w = int(input("Enter width:"))
+    area == l * w
+    print(f'The area is {area}')
+
+
+
+import math
+
+def circum(radius):
+    return 2 * math.pi * radius
+
